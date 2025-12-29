3cb7b1e55704a5efd74883d0e1f834b16d95a4ee
šłîçk phäñtøm <ynwghosted@icloud.com>
2025-12-29 13:42:32 +0000

    Upload New File

---
+++ Test-Projects/Movie-tracker-bot/handlers/sponsor.rb
@@ -0,0 +1,14 @@
+class SponsorHandler
+     def initialize(bot)
+      @bot = bot
+       start_sponsoring_div
+     end 
+    def start_sponsoring_div
+        @bot.hears("🤠sponsor") do |ctx|
+          ctx.typing
+             sleep 5
+      ctx.reply(" ultimately your application will be viewed by **board**" parse_mode: 'Markdown')
+      ctx.reply("chat with our board @darksnipe")
+    end 
+   end 
+  end 
